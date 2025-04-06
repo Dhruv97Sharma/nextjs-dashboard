572ca2deb567a9bece4f7e88ade19ede0ee82fe2
Dhruv97Sharma <ddhuvsharma1997@gmail.com>
2025-04-06 13:51:36 +0530

    Added config in next.config.ts to disable build failures due to eslint warnings and errors

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     ppr: 'incremental'
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   }
 };
 
